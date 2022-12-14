2a49b0d3d601f09e652a5271007dc0047f502428
Alencar <alencarantonio@hotmail.com.br>
2022-12-14 19:59:15 -0300

    Add create user data method

--- lib/src/authentication/create_account/create_account_bloc.dart
+++ lib/src/authentication/create_account/create_account_bloc.dart
@@ -2,6 +2,7 @@ import 'package:class_finance_app/src/authentication/repository/repository.dart'
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../shared/models/user.dart';
 import 'create_account_state.dart';
 
 class CreateAccountCubit extends Cubit<CreateAccountState> {
@@ -23,6 +24,13 @@ class CreateAccountCubit extends Cubit<CreateAccountState> {
         name: name,
         email: email,
         password: password,
+      );
+
+      await repository.createUserData(
+        user: AppUser(
+          userName: name,
+          userId: repository.getUser()!.uid,
+        ),
       );
 
       emit(CreateAccountStateSuccess());
