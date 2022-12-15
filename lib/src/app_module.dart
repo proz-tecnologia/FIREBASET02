04c52619d70cda9f257d35997ea40ab3553e2c80
Alencar <alencarantonio@hotmail.com.br>
2022-12-14 21:56:59 -0300

    Inject App controller and ModuleRoute

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,16 +1,26 @@
+import 'package:class_finance_app/src/app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'authentication/authentication_module.dart';
+import 'home/home_module.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind<Object>> get binds => [];
+  List<Bind<Object>> get binds => [
+        Bind.lazySingleton<AppController>(
+          (i) => AppController(),
+        ),
+      ];
 
   @override
   List<ModularRoute> get routes => [
         ModuleRoute(
           '/',
           module: AuthenticationModule(),
+        ),
+        ModuleRoute(
+          '/home',
+          module: HomeModule(),
         ),
       ];
 }
